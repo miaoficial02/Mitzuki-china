a7e97996abe8c254fd53e6aa012c6e3a2a0d0419
Its.mia.oficial <hibqbqxdd@gmail.com>
2025-06-12 11:10:53 -0300

    Id.js

--- plugins/Id.js
+++ plugins/Id.js
@@ -1,1 +1,15 @@
+let handler = async (m, { isGroup }) => {
+  if (isGroup) {
+    // Si es grupo, muestra el ID del grupo
+    m.reply(`El ID de este grupo es:\n${m.chat}`)
+  } else {
+    // Si es privado, muestra el ID del usuario
+    m.reply(`Tu ID es:\n${m.sender}`)
+  }
+}
 
+handler.help = ['id']
+handler.tags = ['info']
+handler.command = ['id']
+
+export default handler
